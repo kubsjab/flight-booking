439eca849d9efacc160eba69785298acef4b916d
kubsjab <kubsjab@gmail.com>
2019-12-26 00:07:10 +0100

    #11 Add FlightGenerator actor, change SimulationType to Enum

--- src/main/scala/pl/edu/pw/ii/sag/flightbooking/simulation/OverbookingSimulationGuardian.scala
+++ src/main/scala/pl/edu/pw/ii/sag/flightbooking/simulation/OverbookingSimulationGuardian.scala
@@ -2,10 +2,11 @@ package pl.edu.pw.ii.sag.flightbooking.simulation
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import pl.edu.pw.ii.sag.flightbooking.simulation.SimulationType.SimulationType
 
 object OverbookingSimulationGuardian extends Simulation {
 
-  override val name: String = "overbooking"
+  override val simulationType: SimulationType = SimulationType.OVERBOOKING
 
   def apply(): Behavior[Simulation.Start] = {
     Behaviors.setup[Simulation.Start] { context =>
