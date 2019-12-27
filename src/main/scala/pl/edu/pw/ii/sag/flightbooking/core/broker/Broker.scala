d0756518ca2ca24ba545b203c5e96a174c4f587f
MiHu773 <michal.k.grzeszczyk@gmail.com>
2019-12-27 18:32:59 +0100

    #10 add broker spawning

--- src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/broker/Broker.scala
+++ src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/broker/Broker.scala
@@ -1,18 +1,51 @@
 package pl.edu.pw.ii.sag.flightbooking.core.broker
 
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.Behavior
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.EventSourcedBehavior
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import pl.edu.pw.ii.sag.flightbooking.serialization.CborSerializable
+
+case class BrokerData(brokerId: String, name: String, airlineIds: Set[String])
 
 object Broker {
-  sealed trait Command
-  sealed trait Event
-  final case class State()
+  // command
+  sealed trait Command extends CborSerializable
 
-  def apply(): Behavior[Command] =
-    EventSourcedBehavior[Command, Event, State](
-      persistenceId = PersistenceId.ofUniqueId("broker"),
-      emptyState = State(),
-      commandHandler = (state, cmd) => ???,
-      eventHandler = (state, evt) => ???)
+  // event
+  sealed trait Event extends CborSerializable
+
+  // reply
+  sealed trait CommandReply extends CborSerializable
+  sealed trait OperationResult extends CommandReply
+
+  //state
+  final case class State() extends CborSerializable
+
+  def buildId(customId: String): String = s"broker-$customId"
+
+  def apply(brokerData: BrokerData): Behavior[Command] = {
+    Behaviors.setup { context =>
+      EventSourcedBehavior[Command, Event, State](
+        persistenceId = PersistenceId.ofUniqueId(brokerData.brokerId),
+        emptyState = State(),
+        commandHandler = commandHandler(context),
+        eventHandler = eventHandler(context))
+    }
+  }
+
+  private def commandHandler(context: ActorContext[Command]): (State, Command) => Effect[Event, State] = {
+    (state, cmd) =>
+      cmd match {
+        case _ => Effect.none
+      }
+  }
+
+  private def eventHandler(context: ActorContext[Command]): (State, Event) => State = { (state, event) =>
+    event match {
+      case _ => State()
+    }
+  }
+
 }
+
--- src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/broker/BrokerManager.scala
+++ src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/broker/BrokerManager.scala
@@ -1,19 +1,88 @@
 package pl.edu.pw.ii.sag.flightbooking.core.broker
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.EventSourcedBehavior
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior, ReplyEffect}
+import pl.edu.pw.ii.sag.flightbooking.serialization.CborSerializable
 
 
 object BrokerManager {
-  sealed trait Command
-  sealed trait Event
-  final case class State()
+  // command
+  sealed trait Command extends CborSerializable
+  final case class CreateBroker(brokerData: BrokerData, replyTo: ActorRef[OperationResult]) extends Command
+  final case class GetBroker(brokerId: String, replyTo: ActorRef[BrokerCollection]) extends Command
+  final case class GetBrokers(replyTo: ActorRef[BrokerCollection]) extends Command
+  private final case class TerminateBroker(brokerId: String, broker: ActorRef[Broker.Command]) extends Command
+
+  // event
+  sealed trait Event extends CborSerializable
+  final case class BrokerCreated(BrokerData: BrokerData) extends Event
+  final case class BrokerTerminated(brokerId: String, broker: ActorRef[Broker.Command]) extends Event
+
+  // reply
+  sealed trait CommandReply extends CborSerializable
+  sealed trait OperationResult extends CommandReply
+  final case class BrokerCreationConfirmed(brokerId: String) extends OperationResult
+  final case class Rejected(reason: String) extends OperationResult
+  final case class BrokerCollection(brokers: Map[String, ActorRef[Broker.Command]]) extends CommandReply
+
+  //state
+  final case class State(brokerActors: Map[String, ActorRef[Broker.Command]]) extends CborSerializable
 
   def apply(): Behavior[Command] =
-    EventSourcedBehavior[Command, Event, State](
-      persistenceId = PersistenceId.ofUniqueId("broker-manager"),
-      emptyState = State(),
-      commandHandler = (state, cmd) => ???,
-      eventHandler = (state, evt) => ???)
+    Behaviors.setup { context =>
+      EventSourcedBehavior[Command, Event, State](
+        persistenceId = PersistenceId.ofUniqueId("broker-manager"),
+        emptyState = State(Map.empty),
+        commandHandler = commandHandler(context),
+        eventHandler = eventHandler(context))
+    }
+
+  private def commandHandler(context: ActorContext[Command]): (State, Command) => Effect[Event, State] = {
+    (state, cmd) =>
+      cmd match {
+        case c: CreateBroker => createBroker(context, state, c)
+        case c: TerminateBroker => terminateBroker(context, state, c)
+        case c: GetBroker => getBroker(state, c)
+        case c: GetBrokers => getBrokers(state, c)
+      }
+  }
+
+  private def eventHandler(context: ActorContext[Command]): (State, Event) => State = { (state, event) =>
+    event match {
+      case BrokerCreated(brokerData) =>
+        val broker = context.spawn(Broker(brokerData), brokerData.brokerId)
+        context.watchWith(broker, TerminateBroker(brokerData.brokerId, broker))
+        context.log.info(s"Broker: [${brokerData.brokerId}] has been created")
+        State(state.brokerActors.updated(brokerData.brokerId, broker))
+      case BrokerTerminated(brokerId, broker) =>
+        context.log.info(s"Broker: [${brokerId}] has been terminated")
+        context.unwatch(broker)
+        State(state.brokerActors - brokerId)
+    }
+  }
+
+  private def createBroker(context: ActorContext[Command], state: State, cmd: CreateBroker): ReplyEffect[Event, State] = {
+    state.brokerActors.get(cmd.brokerData.brokerId) match {
+      case Some(_) => Effect.reply(cmd.replyTo)(Rejected(s"Broker - [${cmd.brokerData.brokerId}] already exists"))
+      case None =>
+        Effect
+          .persist(BrokerCreated(cmd.brokerData))
+          .thenReply(cmd.replyTo)(_ => BrokerCreationConfirmed(cmd.brokerData.brokerId))
+    }
+  }
+
+  private def terminateBroker(context: ActorContext[Command], state: State, cmd: TerminateBroker): Effect[Event, State] = {
+    Effect.persist(BrokerTerminated(cmd.brokerId, cmd.broker))
+  }
+
+  private def getBroker(state: State, cmd: GetBroker): ReplyEffect[Event, State] = {
+    Effect.reply(cmd.replyTo)(BrokerCollection(state.brokerActors.view.filterKeys(_ == cmd.brokerId).toMap))
+  }
+
+  private def getBrokers(state: State, cmd: GetBrokers): ReplyEffect[Event, State] = {
+    Effect.reply(cmd.replyTo)(BrokerCollection(state.brokerActors))
+  }
+
 }
