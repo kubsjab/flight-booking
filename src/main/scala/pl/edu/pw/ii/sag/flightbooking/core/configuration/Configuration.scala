218241076b1f3c8e354236a0d17fa98f726e12f3
kubsjab <kubsjab@gmail.com>
2019-12-31 00:21:21 +0100

    #15 Change format of config file

--- src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/configuration/Configuration.scala
+++ src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/configuration/Configuration.scala
@@ -4,11 +4,13 @@ import com.typesafe.config.ConfigFactory
 
 object Configuration {
   val configuration = ConfigFactory.load("application.conf")
-  val airlinesCount = configuration.getInt("akka.configuration.airlinesCount")
-  val minFlightCount = configuration.getInt("akka.configuration.minFlightCount")
-  val maxFlightCount = configuration.getInt("akka.configuration.maxFlightCount")
-  val brokersCount = configuration.getInt("akka.configuration.brokersCount")
-  val clientsCount = configuration.getInt("akka.configuration.clientsCount")
+  val airlinesCount = configuration.getInt("akka.configuration.simulation.airline.count")
+  val brokersCount = configuration.getInt("akka.configuration.simulation.broker.count")
+  val clientsCount = configuration.getInt("akka.configuration.simulation.client.count")
+
+  val initialMinFlightCount = configuration.getInt("akka.configuration.simulation.flight.initial.minCount")
+  val initialMaxFlightCount = configuration.getInt("akka.configuration.simulation.flight.initial.maxCount")
+
   val minAirlinesInBrokerCount: Int = Math.max(airlinesCount - 2, 1)
   val maxAirlinesInBrokerCount: Int = airlinesCount
   val minBrokersInClientCount: Int = Math.max(brokersCount - 5, 2)
