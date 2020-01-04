6e1629aa4bac6b491816bdd97105d974762e45c4
kubsjab <kubsjab@gmail.com>
2020-01-04 22:34:34 +0100

    #15 Split configuration to multiple files

--- src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/configuration/Configuration.scala
+++ src/main/scala/pl/edu/pw/ii/sag/flightbooking/core/configuration/Configuration.scala
@@ -4,23 +4,23 @@ import com.typesafe.config.ConfigFactory
 
 object Configuration {
   val configuration = ConfigFactory.load("application.conf")
-  val airlinesCount = configuration.getInt("akka.configuration.simulation.airline.count")
-  val brokersCount = configuration.getInt("akka.configuration.simulation.broker.count")
-  val clientsCount = configuration.getInt("akka.configuration.simulation.client.count")
+  val airlinesCount = configuration.getInt("configuration.simulation.airline.count")
+  val brokersCount = configuration.getInt("configuration.simulation.broker.count")
+  val clientsCount = configuration.getInt("configuration.simulation.client.count")
 
-  val initialMinFlightCount = configuration.getInt("akka.configuration.simulation.flight.initial.minCount")
-  val initialMaxFlightCount = configuration.getInt("akka.configuration.simulation.flight.initial.maxCount")
-  val flightSchedulerEnabled = configuration.getBoolean("akka.configuration.simulation.flight.scheduler.enabled")
-  val flightSchedulerDelay = configuration.getInt("akka.configuration.simulation.flight.scheduler.delay")
-  val schedulerMinFlightCount = configuration.getInt("akka.configuration.simulation.flight.scheduler.minCount")
-  val schedulerMaxFlightCount = configuration.getInt("akka.configuration.simulation.flight.scheduler.maxCount")
+  val initialMinFlightCount = configuration.getInt("configuration.simulation.flight.initial.minCount")
+  val initialMaxFlightCount = configuration.getInt("configuration.simulation.flight.initial.maxCount")
+  val flightSchedulerEnabled = configuration.getBoolean("configuration.simulation.flight.scheduler.enabled")
+  val flightSchedulerDelay = configuration.getInt("configuration.simulation.flight.scheduler.delay")
+  val schedulerMinFlightCount = configuration.getInt("configuration.simulation.flight.scheduler.minCount")
+  val schedulerMaxFlightCount = configuration.getInt("configuration.simulation.flight.scheduler.maxCount")
 
   val minAirlinesInBrokerCount: Int = Math.max(airlinesCount - 2, 1)
   val maxAirlinesInBrokerCount: Int = airlinesCount
   val minBrokersInClientCount: Int = Math.max(brokersCount - 5, 2)
   val maxBrokersInClientCount: Int = brokersCount
 
-  val flightRoutesCount: Int = configuration.getInt("akka.configuration.simulation.flight.generation.routes")
-  val cityFileName: String = configuration.getString("akka.configuration.simulation.flight.generation.citySourceFile")
-  val planeFileName: String = configuration.getString("akka.configuration.simulation.flight.generation.planeSourceFile")
+  val flightRoutesCount: Int = configuration.getInt("configuration.simulation.flight.generation.routes")
+  val cityFileName: String = configuration.getString("configuration.simulation.flight.generation.citySourceFile")
+  val planeFileName: String = configuration.getString("configuration.simulation.flight.generation.planeSourceFile")
 }
