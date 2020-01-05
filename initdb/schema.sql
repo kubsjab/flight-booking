e0aa4be071d26bd9224da2b0e7f3e60f40f33545
kubsjab <kubsjab@gmail.com>
2020-01-05 19:27:53 +0100

    #7 Add Postgresql Materialized views

--- initdb/schema.sql
+++ initdb/schema.sql
@@ -25,7 +25,6 @@ CREATE TABLE IF NOT EXISTS snapshot (
 );
 
 
-
 -- FUNCTIONS
 
 CREATE OR REPLACE FUNCTION  bytea_to_json(msg bytea) RETURNS json AS $$
@@ -48,3 +47,101 @@ BEGIN
             from position('{' in encoded_msg)))::jsonb;
 END;$$
     LANGUAGE PLPGSQL;
+
+
+-- MATERIALIZED VIEWS
+
+-- managers_view
+
+-- Contains information about creating clients, airlines and brokers.
+-- To refresh run:
+--     REFRESH MATERIALIZED VIEW CONCURRENTLY managers_view;
+
+DROP MATERIALIZED VIEW IF EXISTS managers_view;
+
+CREATE MATERIALIZED VIEW managers_view
+AS
+WITH events AS (
+    SELECT ordering,
+           persistence_id,
+           sequence_number,
+           deleted,
+           tags,
+           bytea_to_jsonb(journal.message) as payload
+    FROM journal
+    WHERE persistence_id LIKE 'manager-%'
+)
+SELECT
+    ordering,
+    persistence_id,
+    sequence_number,
+    tags,
+    payload
+FROM events
+WITH DATA;
+
+
+-- bookings_view
+
+-- Contains information about bookings. It contains data for all flights and all flight-booking events like
+-- ticket booked, booking cancelled or overbooked.
+-- To refresh run:
+--     REFRESH MATERIALIZED VIEW CONCURRENTLY bookings_view;
+
+DROP MATERIALIZED VIEW IF EXISTS bookings_view;
+
+CREATE MATERIALIZED VIEW bookings_view
+AS
+WITH events AS (
+    SELECT ordering,
+           persistence_id,
+           sequence_number,
+           deleted,
+           tags,
+           bytea_to_jsonb(journal.message) as payload
+    FROM journal
+    WHERE persistence_id LIKE 'flight-%'
+)
+SELECT
+    ordering,
+    persistence_id,
+    sequence_number,
+    tags,
+    payload ->> 'seatId' AS seat,
+    payload -> 'booking'->> 'bookingId' AS bookingId,
+    CONCAT (payload -> 'booking' -> 'customer'->> 'firstName', ' ', payload -> 'booking' -> 'customer'->> 'lastName') AS customer,
+    payload -> 'booking'->> 'createdDate' AS datetime,
+    payload
+FROM events
+WITH DATA;
+
+
+-- clients_view
+
+-- Contains information about clients and their requests.
+-- To refresh run:
+--     REFRESH MATERIALIZED VIEW CONCURRENTLY clients_view;
+
+DROP MATERIALIZED VIEW IF EXISTS clients_view;
+
+CREATE MATERIALIZED VIEW clients_view
+AS
+WITH events AS (
+    SELECT ordering,
+           persistence_id,
+           sequence_number,
+           deleted,
+           tags,
+           bytea_to_jsonb(journal.message) as payload
+    FROM journal
+    WHERE persistence_id LIKE 'client-%'
+)
+SELECT
+    ordering,
+    persistence_id,
+    sequence_number,
+    tags,
+    payload
+FROM events
+WITH DATA;
+
