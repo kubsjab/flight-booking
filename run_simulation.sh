76086c9e68ae33e2f659b0cfac1623b034602944
pwilcz <przemwil@gmail.com>
2020-01-11 22:03:29 +0100

    Increase heap and stack memory

--- run_simulation.sh
+++ run_simulation.sh
@@ -6,9 +6,7 @@ SIMULATION_TYPE=standard
 
 export DB_NAME=$SIMULATION_TYPE'_db'
 export CONTAINER_NAME="flight-booking-db"
-export SBT_OPTS="-Dslick.db.dbname=$DB_NAME"
+export SBT_OPTS="-Dslick.db.dbname=$DB_NAME -Xmx4G -Xss4m"
 
 eval "./init_db.sh $DB_NAME"
-
-
 sbt "run $SIMULATION_TYPE"
